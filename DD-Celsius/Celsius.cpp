b908597c7ab7cabf62016ad2d932ba9a238b7cca
Leandro <sunleandro010@gmail.com>
2022-06-20 22:27:57 -0300

    subi TP7

---
+++ DD-Celsius/Celsius.cpp
@@ -0,0 +1,27 @@
+#include <cassert>
+#include <iostream>
+//FUNCION MATEMATICA
+//C: R --> R / C(a) = (5/9)*(x - 32)
+//AN: RxRxR --> B / AN(a, b, c) = { b - |c| <= a <= b + |c|
+
+//Prototipo
+double Celsius(double x);
+bool areNear(double x, double y , double = 0.01);
+
+int main() {
+    //Prueba Celsius
+    assert(0 == Celsius(32));
+    assert(10 == Celsius(50.0));
+    //std :: cout << Celsius(50.0);
+    //Prueba areNear
+    assert(areNear(0.9, 1.0, 0.1));
+    assert(areNear(45.99 ,46.0));
+}
+//Definiciones
+double Celsius(double x) {
+    return (5.0/9.0)*(x - 32.0);
+}
+
+bool areNear(double x, double y , double z) {
+    return (y - z) <= x and x <= (y + z);
+}
---
+++ DD-Celsius/readme.md
@@ -0,0 +1,7 @@
+• Demostrar el manejo de funciones y valores punto flotante.\
+• Desarrollar una función que, dada una magnitud en Farehnheit, calcule la\
+ equivalente en Celsius: **Celsius: R --> R / Celsius(a) = (5/9)*(x - 32)**\
+ 1. Escribir el léxico, es decir, la definición matemática de la función.\
+ 2. Escribir las pruebas.\
+ 3. Escribir los prototipos.\
+ 4. Escribir las definiciones.
