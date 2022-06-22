ae63fe57275e3f57ebb0e4b28d457b93f68a8108
Leandro <sunleandro010@gmail.com>
2022-06-22 19:51:23 -0300

    modifique 7

--- DD-Celsius/Celsius.cpp
+++ DD-Celsius/Celsius.cpp
@@ -1,7 +1,7 @@
 #include <cassert>
-#include <iostream>
+
 //FUNCION MATEMATICA
-//C: R --> R / C(a) = (5/9)*(x - 32)
+//C: R --> R / C(x) = (5/9)*(x - 32)
 //AN: RxRxR --> B / AN(a, b, c) = { b - |c| <= a <= b + |c|
 
 //Prototipo
@@ -11,16 +11,18 @@ double modulo(double x);
 
 int main() {
     //Prueba Celsius
-    //assert(0 == Celsius(32));
-    assert(10 == Celsius(50.0));
-    //std :: cout << Celsius(50.0);
+    assert(0 == Celsius(32.0));
+    assert(5.0 == Celsius(41.0));
     //Prueba areNear
     assert(areNear(0.9, 1.0, 0.1));
     assert(areNear(45.99 ,46.0));
+    assert(not areNear(13.78 , 13.80));
+    assert(areNear(-19.99 ,-19.98));
 }
+
 //Definiciones
 double Celsius(double x) {
-    //return 10.0;
+    
     return (5.0/9.0)*(x - 32.0);
 }
 
